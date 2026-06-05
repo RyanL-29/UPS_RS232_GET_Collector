52a7ef65405ac9fd28e39007895201b4db0d4002
Ryan <facebook0209@gmail.com>
2026-06-05 15:58:17 +0800

    Update .gitignore and modify UPSNMC data parsing rules in WA.py

--- app/GET/WA.py
+++ app/GET/WA.py
@@ -13,10 +13,11 @@ WA = UPSNMC_Data_Fetching_Info(
             UPSNMC_Reply_Data_Parse_Rules(key="load_va_1", start_index=18, length=5, data_type="float"),
             UPSNMC_Reply_Data_Parse_Rules(key="load_va_2", start_index=24, length=5, data_type="float"),
             UPSNMC_Reply_Data_Parse_Rules(key="load_va_3", start_index=30, length=5, data_type="float"),
-            UPSNMC_Reply_Data_Parse_Rules(key="load_total_va", start_index=36, length=5, data_type="float"),
-            UPSNMC_Reply_Data_Parse_Rules(key="output_current_1", start_index=42, length=5, data_type="float"),
-            UPSNMC_Reply_Data_Parse_Rules(key="output_current_2", start_index=48, length=5, data_type="float"),
-            UPSNMC_Reply_Data_Parse_Rules(key="output_current_3", start_index=54, length=5, data_type="float"),
+            UPSNMC_Reply_Data_Parse_Rules(key="load_total_watt", start_index=36, length=5, data_type="float"),
+            UPSNMC_Reply_Data_Parse_Rules(key="load_total_va", start_index=42, length=5, data_type="float"),
+            UPSNMC_Reply_Data_Parse_Rules(key="output_current_1", start_index=48, length=5, data_type="float"),
+            UPSNMC_Reply_Data_Parse_Rules(key="output_current_2", start_index=54, length=5, data_type="float"),
+            UPSNMC_Reply_Data_Parse_Rules(key="output_current_3", start_index=60, length=5, data_type="float"),
             UPSNMC_Reply_Data_Parse_Rules(key="load_percent", start_index=66, length=5, data_type="float"),
         ]
     )
